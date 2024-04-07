9c474c81589d7d283d5a3cf6930cb3397a128193
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2024-04-08 00:08:55 +0530

    socket server added

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,32 @@ import express, { Express, Request, Response } from 'express';
 import userRouter from './routes/userRoutes';
 import { errorHandler, notFoundHandler } from './middleware/errorHandler';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 config();
 
 async function startApp() {
   const app: Express = express();
-  const PORT = process.env.PORT || 4000;
-  await connectDB();
-  app.use(cors({ origin: 'http://localhost:3000' }));
+  const httpServer = createServer(app);
+
   app.get('/', (req: Request, res: Response) => {
     res.json('Hello world!');
   });
-  app.use(express.json());
 
-  app.use('/user', userRouter);
+  app.use(cors({ origin: 'http://localhost:3000' }));
+
+  await connectDB();
+
+  // web socket
+  const io = new Server(httpServer);
+
+  app.use(express.json());
+  app.use('/api/user', userRouter);
   app.use(errorHandler);
   app.use(notFoundHandler);
 
-  app.listen(PORT, () => {
+  const PORT = process.env.PORT || 4000;
+  httpServer.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
   });
 }
